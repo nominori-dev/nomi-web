b7c8f04cf101fa66a67d0cbf1f626b8ad7949311
nominori-dev <antonshimchico37@gmail.com>
2023-02-18 22:46:00 +0100

    first attempt to fix build errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import { Roboto } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import React, { FC, Suspense } from 'react'
-import { Canvas} from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber'
 import { TypeAnimation } from 'react-type-animation';
 import { Model } from '../components/Lambo'
 import { Environment, Lightformer, OrbitControls, useProgress, Html } from '@react-three/drei'
@@ -22,7 +22,7 @@ interface GithubStats {
   public_repos?: number
 }
 
-export const Home: FC = ({data}: any) => {
+export const Home: FC = ({ data }: any) => {
 
 
   function Loader() {
@@ -43,37 +43,36 @@ export const Home: FC = ({data}: any) => {
           <div className={styles.tile}>
             <h2>
               <TypeAnimation
-              sequence={['Aleksei Shevtsov', 1000]}
-              speed={45} 
-              wrapper="div"
+                sequence={['Aleksei Shevtsov', 1000]}
+                speed={45}
+                wrapper="div"
               />
             </h2>
-            <p>Software developer and Co-Founder of BDV Foundation, 
-              a company that provides Software as a Service and Outsourcing services. 
+            <p>Software developer and Co-Founder of BDV Foundation,
+              a company that provides Software as a Service and Outsourcing services.
               I&apos;m working with Java-based applications.</p>
           </div>
         </div>
         <div className={styles.scene}>
           <Canvas shadows={false} gl={{ logarithmicDepthBuffer: true, antialias: false, alpha: true }} dpr={[1, 1.5]} camera={{ position: [0, 0, 15], fov: 25 }} className={styles.canvas}>
-            <Suspense fallback={<Loader/>}>
-            <Model rotation={[0, Math.PI / 0.55, 0]} scale={0.018} />
-            <hemisphereLight intensity={1} />
-            
-          <Environment resolution={512}>
-            {/* Ceiling */}
-            <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, -9]} scale={[10, 1, 1]} getObjectsByProperty={undefined} getVertexPosition={undefined} />
-            <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, -6]} scale={[10, 1, 1]} getObjectsByProperty={undefined} getVertexPosition={undefined} />
-            <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, -3]} scale={[10, 1, 1]} getObjectsByProperty={undefined} getVertexPosition={undefined} />
-            <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 0]} scale={[10, 1, 1]} getObjectsByProperty={undefined} getVertexPosition={undefined} />
-            <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 3]} scale={[10, 1, 1]} getObjectsByProperty={undefined} getVertexPosition={undefined} />
-            <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 6]} scale={[10, 1, 1]} getObjectsByProperty={undefined} getVertexPosition={undefined} />
-            <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 9]} scale={[10, 1, 1]} getObjectsByProperty={undefined} getVertexPosition={undefined} />
-            {/* Sides */}
-            <Lightformer intensity={2} rotation-y={Math.PI / 2} position={[-50, 2, 0]} scale={[100, 2, 1]} getObjectsByProperty={undefined} getVertexPosition={undefined} />
-            <Lightformer intensity={2} rotation-y={-Math.PI / 2} position={[50, 2, 0]} scale={[100, 2, 1]} getObjectsByProperty={undefined} getVertexPosition={undefined} />
-          </Environment>
+            <Suspense fallback={<Loader />}>
+              <Model rotation={[0, Math.PI / 0.55, 0]} scale={0.018} />
+              <hemisphereLight intensity={1} />
+              <Environment resolution={512}>
+                {/* Ceiling */}
+                <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, -9]} scale={[10, 1, 1]} getObjectsByProperty={null} getVertexPosition={null} /> 
+                <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, -6]} scale={[10, 1, 1]} getObjectsByProperty={null} getVertexPosition={null} />
+                <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, -3]} scale={[10, 1, 1]} getObjectsByProperty={null} getVertexPosition={null} />
+                <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 0]} scale={[10, 1, 1]} getObjectsByProperty={null} getVertexPosition={null} />
+                <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 3]} scale={[10, 1, 1]} getObjectsByProperty={null} getVertexPosition={null} />
+                <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 6]} scale={[10, 1, 1]} getObjectsByProperty={null} getVertexPosition={null} />
+                <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 9]} scale={[10, 1, 1]} getObjectsByProperty={null} getVertexPosition={null} />
+                {/* Sides */}
+                <Lightformer intensity={2} rotation-y={Math.PI / 2} position={[-50, 2, 0]} scale={[100, 2, 1]} getObjectsByProperty={null} getVertexPosition={null} />
+                <Lightformer intensity={2} rotation-y={-Math.PI / 2} position={[50, 2, 0]} scale={[100, 2, 1]} getObjectsByProperty={null} getVertexPosition={null} />
+              </Environment>
 
-          <OrbitControls autoRotate={true} enablePan={false} enableZoom={false} minPolarAngle={Math.PI / 2.2} maxPolarAngle={Math.PI / 2.2} />
+              <OrbitControls autoRotate={true} enablePan={false} enableZoom={false} minPolarAngle={Math.PI / 2.2} maxPolarAngle={Math.PI / 2.2} />
             </Suspense>
           </Canvas>
         </div>
@@ -85,9 +84,9 @@ export const Home: FC = ({data}: any) => {
           </div>
           <div className={styles.tile}>
             <h2>My skills</h2>
-              <p>Java, Spring Framework (Core, Web, Data)</p>
-              <p>Git, Github, JVM Build Tools (Maven / Gradle)</p>
-              <p>SQL RDBMS (MySQL, MariaDB, PostgreSQL)</p>
+            <p>Java, Spring Framework (Core, Web, Data)</p>
+            <p>Git, Github, JVM Build Tools (Maven / Gradle)</p>
+            <p>SQL RDBMS (MySQL, MariaDB, PostgreSQL)</p>
           </div>
           <div className={styles.tile}>
             <h2>Github stats</h2>
@@ -103,7 +102,7 @@ export const Home: FC = ({data}: any) => {
 }
 
 
-;(Home as PageWithLayoutType).layout = Layout;
+  ; (Home as PageWithLayoutType).layout = Layout;
 
 export async function getServerSideProps() {
   const res = await fetch(`https://api.github.com/users/nominori-dev`)
@@ -117,7 +116,7 @@ export async function getServerSideProps() {
   data.following = resData.following;
   data.location = resData.location;
 
-  return { props: {data}}
+  return { props: { data } }
 }
 
 export default Home;
