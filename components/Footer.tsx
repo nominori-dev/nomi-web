5495652fbe752ac227278700c6e1770c0ea19afd
nominori-dev <antonshimchico37@gmail.com>
2023-03-19 22:44:09 +0100

    moved module css to global

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <footer className="w-full h-20 text-black text-center">
+            <div className="mt-4">
+                <p>Copyright @ 2023 nominori-dev</p>
+                <small>Backed by <a href='https://www.bdv.pw'>bdv.pw</a></small>
+            </div>
+        </footer>
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,33 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: [
-        "./app/**/*.{js,ts,jsx,tsx}",
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
+const plugin = require('tailwindcss/plugin');
 
-        // Or if using `src` directory:
-        "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
+module.exports = {
+        content: [
+            "./app/**/*.{js,ts,jsx,tsx}",
+            "./pages/**/*.{js,ts,jsx,tsx}",
+            "./components/**/*.{js,ts,jsx,tsx}",
+
+            // Or if using `src` directory:
+            "./src/**/*.{js,ts,jsx,tsx}",
+        ],
+        theme: {
+            extend: {},
+        },
+        plugins: [
+                require('tailwindcss-font-inter'),
+                plugin(function({ addVariant, e, postcss }) {
+                        addVariant('firefox', ({ container, separator }) => {
+                                    const isFirefoxRule = postcss.atRule({
+                                        name: '-moz-document',
+                                        params: 'url-prefix()',
+                                    });
+                                    isFirefoxRule.append(container.nodes);
+                                    container.append(isFirefoxRule);
+                                    isFirefoxRule.walkRules((rule) => {
+                                                rule.selector = `.${e(
+                              `firefox${separator}${rule.selector.slice(1)}`
+                            )}`;
+                        });
+                    });
+                }), ],
+        }
