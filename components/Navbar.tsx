4a5188dd47dbad338f22abcebf9f5b25079b4afb
Aleksei Shevtsov <antonshimchico37@gmail.com>
2023-04-05 21:47:29 +0200

    added flowbite to project

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <nav className="sticky top-0 z-10 bg-white backdrop-filter backgrop-blur-lg bg-opacity-30 drop-shadow-lg border-gray-200">
+        <nav className="sticky top-0 z-10 bg-white backdrop-filter backdrop-blur-lg bg-opacity-30 drop-shadow-lg border-gray-200">
             <div className="max-w-5xl mx-auto px-4">
                 <div className="flex items-center justify-between h-16">
-                    <span className="text-2xl text-gray-900 font-semibold">nominori-dev</span>
-                    <div className="flex space-x-4 text-gray-900">
+                    <span className="sm:text-2xl text-gray-900 font-semibold">nominori-dev</span>
+                    <div className="flex space-x-2 text-gray-900">
                         <Link href='/'>main</Link>
                         <Link href='https://github.com/nominori-dev'>github</Link>
                         <Link href='/projects'>projects</Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,32 +2,19 @@
 const plugin = require('tailwindcss/plugin');
 
 module.exports = {
-        content: [
-            "./app/**/*.{js,ts,jsx,tsx}",
-            "./pages/**/*.{js,ts,jsx,tsx}",
-            "./components/**/*.{js,ts,jsx,tsx}",
-
-            // Or if using `src` directory:
-            "./src/**/*.{js,ts,jsx,tsx}",
-        ],
-        theme: {
-            extend: {},
-        },
-        plugins: [
-                require('tailwindcss-font-inter'),
-                plugin(function({ addVariant, e, postcss }) {
-                        addVariant('firefox', ({ container, separator }) => {
-                                    const isFirefoxRule = postcss.atRule({
-                                        name: '-moz-document',
-                                        params: 'url-prefix()',
-                                    });
-                                    isFirefoxRule.append(container.nodes);
-                                    container.append(isFirefoxRule);
-                                    isFirefoxRule.walkRules((rule) => {
-                                                rule.selector = `.${e(
-                              `firefox${separator}${rule.selector.slice(1)}`
-                            )}`;
-                        });
-                    });
-                }), ],
-        }
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./node_modules/flowbite-react/**/*.js",
+        // Or if using `src` directory:
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require("flowbite/plugin"),
+        require('tailwindcss-font-inter'),
+    ],
+}
