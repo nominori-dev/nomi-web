53e9c23a273b53897c17b845533a0d92e7b20b03
nominori-dev <antonshimchico37@gmail.com>
2023-05-06 23:10:56 +0200

    new color pallete

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <nav className="sticky top-0 z-10 bg-white backdrop-filter backdrop-blur-2xl bg-opacity-20 drop-shadow-2xl border-gray-200">
+        <nav className="fixed sticky top-0 z-10 backdrop-filter backdrop-blur-2xl bg-opacity-30 shadow-2xl">
             <div className="max-w-5xl mx-auto px-4">
                 <div className="flex items-center justify-between h-14">
-                    <span className="sm:text-2xl text-gray-900 font-black font-semibold hover:text-gray-800"><Link href='/'>nominori-dev</Link></span>
+                    <span className="sm:text-2xl text-gray-900 font-black font-semibold hover:text-gray-800"><Link href='/'>🛠 nominori-dev</Link></span>
                     <div className="flex space-x-2 text-xl font-semibold text-gray-900 underline">
                         <Link className="hover:text-gray-600 hover:transition-all" href='/'>main</Link>
                         <Link className="hover:text-gray-600 hover:transition-all" href='https://github.com/nominori-dev'>github</Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,9 @@
 // @ts-nocheck
 import Head from 'next/head'
-import React, { FC, Suspense } from 'react'
-import { Canvas } from '@react-three/fiber'
-import { Model } from '../components/Lambo'
-import { Environment, Lightformer, OrbitControls, useProgress, Html } from '@react-three/drei'
+import React, { FC } from 'react'
 import Layout from '../components/layout/Layout'
 import PageWithLayoutType from '../components/layout/PageWithLayoutType'
 import Link from 'next/link'
-import HeroTile from '../components/hero/HeroTile'
 
 interface GithubStats {
   name?: string,
@@ -19,12 +15,6 @@ interface GithubStats {
 
 export const Home: FC = ({ data }: any) => {
 
-
-  function Loader() {
-    const { progress } = useProgress()
-    return <Html center>{progress} % loaded</Html>
-  }
-
   return (
     <>
       <Head>
@@ -33,17 +23,17 @@ export const Home: FC = ({ data }: any) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className='h-screen flex items-center justify-center'>
+      <main className='h-screen flex flex-col items-center justify-center'>
+        <div className='flex flex-col text-center space-y-2 pb-8'>
+          <h1 class="mb-2 text-6xl font-bold tracking-tight text-gray-900 hover:underline">Aleksei Shevtsov</h1>
+          <p class="mb-3 text-xl max-w-4xl font-normal text-gray-700">Software developer and Co-Founder of BDV Foundation,
+            a company that provides Software as a Service and Outsourcing services.
+            I&apos;m working with Java-based applications.</p>
+        </div>
         <div className='pb-24'>
-          <div className="max-w-xs md:max-w-lg p-6 bg-white backdrop-filter backdrop-blur-lg bg-opacity-50 rounded-xl drop-shadow-2xl">
-            <Link href="#">
-              <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 hover:underline">Aleksei Shevtsov</h5>
-            </Link>
-            <p class="mb-3 font-normal text-gray-600">Software developer and Co-Founder of BDV Foundation,
-              a company that provides Software as a Service and Outsourcing services.
-              I&apos;m working with Java-based applications.</p>
+          <div className="max-w-xs md:max-w-lg p-6 bg-white backdrop-filter backdrop-blur-lg bg-opacity-40 rounded-xl shadow-2xl shadow-purple-400">
             <div>
-              <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 hover:underline">Skills</h5>
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">📚 <span className=' hover:underline'>Skills</span></h5>
               <div>
                 <p class="mb-3 font-normal text-gray-600">Java + Spring Framework (CRUD Applications, REST API's, MVC, Microservices).</p>
                 <p class="mb-3 font-normal text-gray-600">TypeScript & JavaScript (React, NextJS, NestJS, Vue).</p>
@@ -51,13 +41,17 @@ export const Home: FC = ({ data }: any) => {
               </div>
             </div>
             <div>
-              <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 hover:underline">Contact</h5>
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">📞 <span className='hover:underline'>Contact</span></h5>
               <div>
                 <p class="mb-3 font-normal text-gray-600">You can reach me by this e-mail: <Link href="mailto:nominori@bdv.pw" class="inline-flex items-center text-blue-900 hover:underline">
                   nominori@bdv.pw
                   <svg class="w-5 h-5 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
-                </Link> </p>
-
+                </Link> <br />
+                  More details here -&gt; <Link href="https://co.bdv.pw/contact" class="inline-flex items-center text-blue-900 hover:underline">
+                    co.bdv.pw/contact
+                    <svg class="w-5 h-5 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
+                  </Link>
+                </p>
               </div>
             </div>
           </div>
