61e63d9b8d7b621b8dfeb9adb7aeb633d799b3da
nominori-dev <antonshimchico37@gmail.com>
2023-05-06 23:18:37 +0200

    removed unused dependencies

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,3 @@
-import { motion } from "framer-motion";
 import { useRouter } from "next/router";
 import { ReactNode } from "react";
 
@@ -6,34 +5,10 @@ type Props = {
     children: ReactNode
 }
 
-const variants = {
-    initial: {
-        opacity: 0,
-        y: 8,
-    },
-    enter: {
-        opacity: 1,
-        y: 0,
-        transition: {
-            duration: 0.4,
-            ease: [0.91, 1, 0.88, 1],
-        },
-    },
-}
-
 const Layout = ({ children }: Props): JSX.Element => {
-    const { asPath } = useRouter();
     return (
         <div>
-            <motion.main
-                key={asPath}
-                initial="initial"
-                animate="enter"
-                variants={variants}
-                transition={{ type: 'linear' }}
-            >
-                {children}
-            </motion.main>
+            {children}
         </div>
     );
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import '../styles/globals.css'
 import PageWithLayoutType from '../components/layout/PageWithLayoutType'
-import { AnimatePresence } from 'framer-motion'
 import { useRouter } from "next/router";
 
 type AppLayoutProps = {
@@ -12,10 +11,8 @@ export default function App({ Component, pageProps }: AppLayoutProps) {
   const router = useRouter();
   const NavLayout = Component.layout || ((children) => <>{children}</>)
   return (
-    <AnimatePresence mode='wait' initial={false} onExitComplete={() => window.scrollTo(0, 0)}>
-      <NavLayout>
-        <Component {...pageProps} key={router.route} />
-      </NavLayout>
-    </AnimatePresence>
+    <NavLayout>
+      <Component {...pageProps} key={router.route} />
+    </NavLayout>
   )
 }
